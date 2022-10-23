e861cbb878dcd52f376de95dd77a809c0e5a8e29
Sean <seanpatrickllera@yahoo.ca>
2022-10-22 23:26:05 -0400

    Implemented frontend in typescript that interacts with local weatherforecast api

--- MergeSharpWebAPI/Program.cs
+++ MergeSharpWebAPI/Program.cs
@@ -1,6 +1,17 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000");
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +28,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,31 +14,32 @@ type resultProps = {
 function App() {
     const [result, setResult] = useState<resultProps[]>([]);
 
-    useEffect(() => {
-        const api = async () => {
-            const data = await fetch("https://localhost:7009/weatherforecast", {
-                method: "GET"
-            });
-            const jsonData = await data.json();
-            setResult(jsonData.results);
-        };
 
-        api();
+    useEffect(() => {
+        fetch('https://localhost:7009/weatherforecast')
+            .then(response => response.json())
+            .then(res => setResult(res))
+            .catch(err => console.log(err))
     }, []);
+
+    const result2 = [{ "date": "2022-10-23T22:27:55.9961759-04:00", "temperatureC": 52, "temperatureF": 125, "summary": "Chilly" }, { "date": "2022-10-24T22:27:55.9961963-04:00", "temperatureC": 33, "temperatureF": 91, "summary": "Scorching" }, { "date": "2022-10-25T22:27:55.9961975-04:00", "temperatureC": 38, "temperatureF": 100, "summary": "Chilly" }, { "date": "2022-10-26T22:27:55.9961983-04:00", "temperatureC": 33, "temperatureF": 91, "summary": "Warm" }, { "date": "2022-10-27T22:27:55.9961991-04:00", "temperatureC": -7, "temperatureF": 20, "summary": "Mild" }]
+
     return (
         <div className="App">
-            <h1>
+            <table>
                 {result.map((value) => {
                     return (
-                        <div>
-                            <div>{value.date}</div>
-                            <div>{value.temperatureC}</div>
-                            <div>{value.temperatureF}</div>
-                            <div>{value.summary}</div>
-                        </div>
+                        <tbody>
+                            <tr>
+                                <td>{value.date}</td>
+                                <td>{value.temperatureC}</td>
+                                <td>{value.temperatureF}</td>
+                                <td>{value.summary}</td>
+                            </tr>
+                        </tbody>
                     );
                 })}
-            </h1>
+            </table>
         </div>
     );
 }
